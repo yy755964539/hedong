de83e24f56f4eb7b6e78b3c9c4ae2fa0178ab36d
Administrator <Administrator@I75HHG3YG5XNR60>
2018-03-21 17:34:02 +0800

    第二次提交

---
+++ src/main/java/com/peixun/Peixun.java
@@ -0,0 +1,13 @@
+package com.peixun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Peixun {
+
+	public static void main(String[]args)
+	{
+		SpringApplication.run(Peixun.class, args);
+	}
+}
